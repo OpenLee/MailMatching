d102017de5520584444db097cf9c3bdf7e300c74
徐林10014 <mac020@xl10014deMac-mini-5.local>
2016-03-01 10:11:38 +0800

    针对邮箱的优先级匹配

---
+++ MailSuffix-master/MailSuffix/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MailSuffix
+//
+//  Created by XL10014 on 15/10/28.
+//  Copyright © 2015年 XL10014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
