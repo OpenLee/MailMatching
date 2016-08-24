6ab5166fbd4deb6c16250e7d79c90f4a489aa0e9
徐林10014 <mac020@xl10014deMac-mini-5.local>
2016-08-24 16:23:11 +0800

    邮箱匹配功能 进行优化

--- MailSuffix-master/MailSuffix/ViewController.m
+++ MailSuffix-master/MailSuffix/ViewController.m
@@ -7,13 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "XLTextField.h"
 
 @interface ViewController ()
 
-@property(nonatomic,strong) IBOutlet UITextField *mailTextField;   //邮箱输入框
-@property(nonatomic,strong) IBOutlet UILabel *suffixLbl;           //配合输入框显示的label
-@property(nonatomic,strong) NSMutableArray *emailArray;        //需要做匹配的后缀
-@property(nonatomic,strong) NSString *email;                   //完整的邮箱
+@property (weak, nonatomic) IBOutlet XLTextField *textField;
 
 @end
 
@@ -22,10 +20,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.emailArray = [NSMutableArray arrayWithObjects:@"@qq.com",@"@163.com",@"@126.com",@"@yahoo.com",@"@139.com",@"@henu.com", nil];
-    self.email = [[ NSMutableString alloc]initWithCapacity:0];
-    self.suffixLbl.hidden = NO;
+    
+    /**
+     手动frame创建XLTextField
+     */
+    //    XLTextField * Field = [[XLTextField alloc] initWithFrame:CGRectMake(100, 200, 200, 30) fontSize:12];
+    //    Field.placeholder = @"输入邮箱地址";
+    //    Field.mailTypeArray = [NSMutableArray arrayWithObjects:@"@qq.com",@"@163.com",@"@126.com",@"@yahoo.com",@"@139.com",@"@henu.com", nil];
+    ////    Field.mailMatchColor = [UIColor redColor];
+    //    [self.view addSubview:Field];
+    
+    
+    /**
+     利用xib创建XLTextField
+     */
+    self.textField.mailTypeArray = [NSMutableArray arrayWithObjects:@"@qq.com",@"@163.com",@"@126.com",@"@yahoo.com",@"@139.com",@"@henu.com", nil];
+    //    self.textField.mailMatchColor = [UIColor redColor];
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -33,65 +46,10 @@
 }
 
 
-- (void)textFieldDidEndEditing:(UITextField *)textField{             // may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
-    textField.text = self.email;
-    self.suffixLbl.text = @"";
-    NSLog(@"%@",self.email);
+- (IBAction)tapEnd:(id)sender {
+    [self.textField resignFirstResponder];
 }
 
 
-
-- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{   // return NO to not change text
-    
-    //获取完整的输入文本
-    NSString *completeStr = [textField.text stringByReplacingCharactersInRange:range withString:string];
-    //以@符号分割文本
-    NSArray *temailArray = [completeStr componentsSeparatedByString:@"@"];
-    //获取邮箱前缀
-    NSString *emailString = [temailArray firstObject];
-    
-    //邮箱匹配 没有输入@符号时 用@匹配
-    NSString *matchString = @"@";
-    if(temailArray.count > 1){
-        //如果已经输入@符号 截取@符号以后的字符串作为匹配字符串
-        matchString = [completeStr substringFromIndex:emailString.length];
-    }
-    //匹配邮箱 得到所有跟当前输入匹配的邮箱后缀
-    NSMutableArray *suffixArray = [self checkEmailStr:matchString];
-    
-    //边界控制 如果没有跟当前输入匹配的后缀置为@""
-    NSString *fixStr = suffixArray.count > 0 ? [suffixArray firstObject] : @"";
-    //将lblEmail部分字段隐藏
-    NSInteger cutLenth = suffixArray.count > 0 ? completeStr.length : emailString.length;
-    
-    //最终的邮箱地址
-    self.email = fixStr.length > 0 ? [NSString stringWithFormat:@"%@%@",emailString,fixStr] : completeStr;
-    //self.strEmail = [NSString stringWithFormat:@"%@%@",emailString,fixStr];
-    
-    //设置lblEmail 的attribute
-    NSMutableAttributedString *attributeString = [[NSMutableAttributedString alloc]initWithString:[NSString stringWithFormat:@"%@%@",emailString,fixStr]];
-    [attributeString addAttribute:NSForegroundColorAttributeName value:[UIColor clearColor] range:NSMakeRange(0,cutLenth)];
-    self.suffixLbl.attributedText = attributeString;     
-    
-    //清空文本框内容时 隐藏lblEmail
-    if(completeStr.length == 0){
-        self.suffixLbl.text = @"";
-        self.email = @"";
-    }
-    return YES;
-}
-
-- (NSMutableArray *)checkEmailStr:(NSString *)string{
-    NSMutableArray *filterArray = [NSMutableArray arrayWithCapacity:0];
-    for (NSString *str in self.emailArray) {
-        if([str hasPrefix:string]){
-            [filterArray addObject:str];
-        }
-    }
-    return filterArray;
-}
-- (IBAction)gestureTap:(UITapGestureRecognizer *)sender {
-    [self.mailTextField resignFirstResponder];
-}
 
 @end
